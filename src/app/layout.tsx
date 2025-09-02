cf87796dee2da4265a12542c11c99c5213b6516d
daniil-b2be <dan.developer@born2be.media>
2025-09-02 09:51:27 +0300

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,24 @@
-import type { Metadata } from "next";
-import { Montserrat, Outfit } from "next/font/google";
+import type { Metadata } from 'next';
+import { Montserrat, Outfit } from 'next/font/google';
 
-import "@/shared/lib/styles/null.scss";
-import "@/shared/lib/styles/base.scss";
+import '@/shared/lib/styles/null.scss';
+import '@/shared/lib/styles/base.scss';
 
-import { Header } from "@/shared/ui/components/header/Header";
-import { Footer } from "@/shared/ui/components/footer/Footer";
+import './globals.css';
 
 const montserrat = Montserrat({
-  variable: "--font-montserrat",
-  subsets: ["latin"],
+  variable: '--font-montserrat',
+  subsets: ['latin'],
 });
 
 const outfit = Outfit({
-  variable: "--font-outfit",
-  subsets: ["latin"],
+  variable: '--font-outfit',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Coinsmax",
-  description: "",
+  title: 'Coinsmax',
+  description: '',
 };
 
 export default function RootLayout({
@@ -30,9 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${montserrat.variable} ${outfit.variable}`}>
-        <Header />
         {children}
-        <Footer />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
-import { FeatureSection } from "./components/FeatureSection/FeatureSection";
-import { HeroSection } from "./components/HeroSection/HeroSection";
-import { FormSection } from "./components/FormSection/FormSection";
-import { PreviewSection } from "./components/PreviewSection/PreviewSection";
-
 export default function Home() {
   return (
-    <>
-      <HeroSection />
-      <FeatureSection />
-      <PreviewSection />
-      <FormSection />
-    </>
+    <main>
+      <p className="text-white">Main</p>
+    </main>
   );
 }
