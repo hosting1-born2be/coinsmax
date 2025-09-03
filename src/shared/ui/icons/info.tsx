efc07ed9d1dcb3b1972baffebb96718ce45bc3f5
daniil-b2be <dan.developer@born2be.media>
2025-09-03 16:30:02 +0300

    redesign

---
+++ src/shared/ui/icons/info.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+export const InfoIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="13"
+    height="14"
+    viewBox="0 0 13 14"
+    fill="none"
+  >
+    <path
+      d="M6.5 13C9.81371 13 12.5 10.3137 12.5 7C12.5 3.68629 9.81371 1 6.5 1C3.18629 1 0.5 3.68629 0.5 7C0.5 10.3137 3.18629 13 6.5 13Z"
+      fill="white"
+      fillOpacity="0.1"
+      stroke="white"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M6.5 9.66675H6.50625V9.673H6.5V9.66675Z"
+      stroke="white"
+      strokeWidth="2"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M6.5 6.99992V4.33325"
+      stroke="white"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
