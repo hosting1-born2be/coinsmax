a47a11e7a29ea531d005bd5b200381dd79f4c34b
Daniil <dan.developer@born2be.media>
2025-09-04 18:30:43 +0300

    [feature/contact-page]: Contact Page (Bg + Translations) (#9)

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,6 +1,6 @@
 import { defineRouting } from 'next-intl/routing';
 
 export const routing = defineRouting({
-  locales: ['en', 'de', 'it', 'sk'],
+  locales: ['en', 'sk'],
   defaultLocale: 'sk',
 });
