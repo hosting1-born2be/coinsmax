4df5514937dd0bfee4f51c6b7720f5b040d680d4
Daniil <dan.developer@born2be.media>
2025-09-08 18:18:04 +0300

    [feature/default-lng]: Default Lng (#17)

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,5 +3,6 @@ import { defineRouting } from 'next-intl/routing';
 export const routing = defineRouting({
   locales: ['sk', 'en'],
   localePrefix: 'as-needed',
+  localeDetection: false,
   defaultLocale: 'sk',
 });
