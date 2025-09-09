ffcebcdb3968ab298a1c472f15e2cc64916ef61c
Daniil <dan.developer@born2be.media>
2025-09-09 16:31:09 +0300

    [feature/copyright-design]: Navigation (#21)

--- src/shared/ui/components/info-card.tsx
+++ src/shared/ui/components/info-card.tsx
@@ -3,6 +3,7 @@
 import type { ReactNode } from 'react';
 import Image from 'next/image';
 
+import { cn } from '@/shared/lib/utils/cn';
 import { Text } from '@/shared/ui/kit/text';
 import { Title } from '@/shared/ui/kit/title';
 
@@ -10,20 +11,24 @@ export const InfoCard = ({
   imgUrl,
   text,
   title,
+  alignImage = false,
 }: {
   imgUrl: string;
   title?: ReactNode;
   text: string;
+  alignImage?: boolean;
 }) => (
   <article className="flex flex-1 flex-col gap-8 rounded-4xl bg-[#222] p-8">
-    <Image
-      className="my-auto"
-      src={imgUrl}
-      alt="info-img"
-      width={96}
-      height={96}
-      unoptimized
-    />
+    <div className="h-[96px]">
+      <Image
+        className={cn(alignImage && 'py-auto')}
+        src={imgUrl}
+        alt="info-img"
+        width={96}
+        height={96}
+        unoptimized
+      />
+    </div>
     <div className="mb-auto flex flex-col gap-2">
       {title && (
         <Title as="h3" size="3xl">
