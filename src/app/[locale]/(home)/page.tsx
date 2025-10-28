711b48a7a1c2666ef6f59986db204e055d8f9ef9
Daniil <dan.developer@born2be.media>
2025-10-28 12:56:25 +0200

    [feature/remove-meta-home]: Remove Meta Home (#38)

--- src/app/[locale]/(home)/page.tsx
+++ src/app/[locale]/(home)/page.tsx
@@ -12,22 +12,6 @@ import {
   WhyCoinsmax,
 } from './components';
 
-export const metadata: Metadata = {
-  title: 'Your Regulated Crypto Exchange  | Coinsmax',
-  description:
-    'Discover how to exchange Bitcoin and Ethereum for fiat, or vice versa. All transactions are processed in line with regulatory requirements, directly from your wallet.',
-  openGraph: {
-    title: 'Your Regulated Crypto Exchange  | Coinsmax',
-    description:
-      'Discover how to exchange Bitcoin and Ethereum for fiat, or vice versa. All transactions are processed in line with regulatory requirements, directly from your wallet.',
-  },
-  twitter: {
-    title: 'Your Regulated Crypto Exchange  | Coinsmax',
-    description:
-      'Discover how to exchange Bitcoin and Ethereum for fiat, or vice versa. All transactions are processed in line with regulatory requirements, directly from your wallet.',
-  },
-};
-
 export default function Home() {
   return (
     <main>
